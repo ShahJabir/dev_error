afad2559366067f2fa4274dd6ed29ba2a68ea37d
Shah Jabir <shahjabirtaqi@gmail.com>
2024-11-23 23:57:12 +0600

    fix: Navbar Title and logo alt fixed

--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -19,10 +19,10 @@ const Navbar = () => {
             src="/assets/images/site-logo.svg"
             width={23}
             height={23}
-            alt="DevFlow"
+            alt="DevError"
           />
           <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-            Dev <span className="text-primary-500">Overflow</span>
+            Dev <span className="text-primary-500">Error</span>
           </p>
         </Link>
         {/* GlobalSearch */}
